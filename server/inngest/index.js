99d87b7294994205eef35c339b6a6d1524eb525f
sarvagya80 <adityapathak8081@gmail.com>
2025-08-13 15:04:35 +0530

    added

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
+import Connection from "../models/Connection.js";
+import sendEmail from "../configs/nodemailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "SarvTribe" });
@@ -58,10 +60,88 @@ const syncUserDeletion =inngest.createFunction(
         await User.findByIdAndDelete(id)
     }
 )
+//ingest function to send reminder when connection request is made
+
+const sendNewConnectionRequestReminder = inngest.createFunction(
+    {id:'send-new-connection-request-reminder'},
+        {event:'app/connection-request'},
+         async ({event, step}) => {
+         const { connectionId } = event.data;
+
+  await step.run("send-connection-request-mail", async () => {
+    const connection = await Connection.findById(connectionId)
+      .populate("from_user_id")
+      .populate("to_user_id");
+
+    const subject = "New Connection Request";
+    const body = `
+      <div style="font-family: Arial, sans-serif; padding: 20px;">
+        <h2>Hi ${connection.to_user_id.full_name},</h2>
+        <p>You have a new connection request from 
+          <strong>${connection.from_user_id.full_name}</strong> 
+          (@${connection.from_user_id.username}).
+        </p>
+        <p>
+          Click 
+          <a href="${process.env.FRONTEND_URL}/connections" style="color: #106981;">
+            here
+          </a> 
+          to accept or reject the request.
+        </p>
+        <br/>
+        <p>Thanks,<br/>sarrvtribe Stay Connected</p>
+      </div>
+    `;
+      await sendEmail({
+      to: connection.to_user_id.email,
+      subject,
+     body
+    });
+
+  });
+  const in24Hours = new Date(Data.now()+24 *60*60*1000)
+  await step.sleepUntil("waite-for-24-hours",in24Hours);
+  await step.run('send-connection-request-reminder' ,async()=>
+{
+    const connection=await Connection.findById(connectionId).populate('from_user_id to_user_id');
+    if(connection.status==='accepted'){
+        return {message:'Already accepted'}
+    }
+    const subject = "New Connection Request";
+    const body = `
+      <div style="font-family: Arial, sans-serif; padding: 20px;">
+        <h2>Hi ${connection.to_user_id.full_name},</h2>
+        <p>You have a new connection request from 
+          <strong>${connection.from_user_id.full_name}</strong> 
+          (@${connection.from_user_id.username}).
+        </p>
+        <p>
+          Click 
+          <a href="${process.env.FRONTEND_URL}/connections" style="color: #106981;">
+            here
+          </a> 
+          to accept or reject the request.
+        </p>
+        <br/>
+        <p>Thanks,<br/>sarrvtribe Stay Connected</p>
+      </div>
+    `;
+      await sendEmail({
+      to: connection.to_user_id.email,
+      subject,
+     body
+    });
+ return{message:'Reminder sent.' }
+
+})
+});
+    
+
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
+    sendNewConnectionRequestReminder
 ];
