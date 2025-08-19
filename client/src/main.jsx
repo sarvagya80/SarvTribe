7e76ed66c22175ffd86708fbb30b9c63cf95633e
sarvagya80 <adityapathak8081@gmail.com>
2025-08-19 21:55:05 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ClerkProvider } from '@clerk/clerk-react';
 import { Provider } from 'react-redux';
-import { store } from './app/Store.js'; // ✅ Store
+import { store } from './app/store.js'; // ✅ Store
 import App from './App.jsx';
 import './index.css';
 
